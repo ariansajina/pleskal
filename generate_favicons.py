190d479500102118b2f465dc0e712a0e8488bd58
Arian Šajina <14201989+ariansajina@users.noreply.github.com>
2026-03-27 00:44:43 +0100

    format

--- generate_favicons.py
+++ generate_favicons.py
@@ -7,7 +7,9 @@ src = "static/images/logo.png"
 img = Image.open(src).convert("RGBA")
 
 img.resize((32, 32), Image.Resampling.LANCZOS).save("static/images/favicon-32x32.png")
-img.resize((180, 180), Image.Resampling.LANCZOS).save("static/images/apple-touch-icon.png")
+img.resize((180, 180), Image.Resampling.LANCZOS).save(
+    "static/images/apple-touch-icon.png"
+)
 
 print("Generated static/images/favicon-32x32.png")
 print("Generated static/images/apple-touch-icon.png")
